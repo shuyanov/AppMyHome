2593273a9ae9f92bf31e547aefd10fe9dfd34e70
sweet62 <maks_shuyanov@mail.ru>
2022-10-29 11:45:06 +0300

    addUser

---
+++ lib/Utils/UserPerefer.dart
@@ -0,0 +1,9 @@
+// import 'package:command_flutter/Model/User.dart';
+//
+// class UserPereferences{
+//   static const myUser = User(
+//       imagePath: imagePath,
+//       name: name,
+//       email: email,
+//       about: about)
+// }
