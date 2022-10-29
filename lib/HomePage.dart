623c0a3da6df7d721776a332f970395c56bad628
sweet62 <maks_shuyanov@mail.ru>
2022-10-29 11:09:00 +0300

    add
    Otkat

---
+++ lib/HomePage.dart
@@ -0,0 +1,88 @@
+import 'package:command_flutter/Widget/Bar/Profile.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  static const _navigationBarItems = <BottomNavigationBarItem>[
+    BottomNavigationBarItem(
+      icon: Icon(Icons.home),
+      label: 'Home',
+      backgroundColor: Colors.blueAccent,
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.message),
+      label: 'Message',
+      backgroundColor: Colors.blue,
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.person),
+        label: 'Profile',
+        backgroundColor: Colors.blue,
+    ),
+  ];
+
+  static const List<Widget> _pages = <Widget>[
+    Text('Test1'),
+    Text('Test2'),
+    ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        leading: IconButton(
+          icon: const Icon(Icons.add_alert_rounded),
+          tooltip: 'Уведомления',
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute<void>(
+                builder: (BuildContext){
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: const Text('Уведомления'),
+                      backgroundColor: Colors.blueAccent,
+                    ),
+                  );
+                }
+            ));
+          },
+        )
+      ),
+      body: Center(
+        child: _pages.elementAt(_currentIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.newspaper),
+            label: 'Новости',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat_outlined),
+            label: 'Чаты',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Личный Кабинет',
+          ),
+        ],
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
