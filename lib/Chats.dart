7a3437999a289820b4d8ee8a525446414e8a1cf2
HubOl01 <kuloleg54@gmail.com>
2022-10-30 03:46:42 +0300

    new chat design

--- lib/Chats.dart
+++ lib/Chats.dart
@@ -13,44 +13,121 @@ class Chats extends StatefulWidget {
   @override
   State<Chats> createState() => _ChatsState();
 }
+
 class _ChatsState extends State<Chats> {
+  String name1 = 'Общий чат';
+  String name2 = 'Подъезд №2';
+  String name3 = '\"УК\"';
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-        body: SafeArea(
-           child: Column(
-             children: [
-              ListTile(
-                  leading: CircleAvatar(
-                    radius: 20.0,
-                    backgroundImage: NetworkImage(generalUrlAvatar),
-                  ),
-                  title: Text('Общий чат'),
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => GeneralChatsPage()
-                        
-                        // GeneralPage(
-                        //     currentUserId: myId,
-                        //     friendId: idGeneral,
-                        //     friendName: generalName,
-                        //     friendImage: generalUrlAvatar)
+      body: Column(children: [
+        // Container(
+        //   child: Container(
+        //       padding: EdgeInsets.all(10),
+        //       child: Text(
+        //         "Чаты",
+        //         style: TextStyle(fontSize: 30, color: Colors.white),
+        //       )),
+        //   height: size.height * 0.3,
+        //   width: size.height,
+        //   color: Colors.blueAccent,
+        // ),
+        Stack(
+          children: [
+          Positioned(
+              top: 0,
+              bottom: size.height * 0.4,
+            child: Container(
+              child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    "Чаты",
+                    style: TextStyle(fontSize: 30, color: Colors.white),
+                  )),
+              height: size.height * 0.3,
+              width: size.height,
+              color: Colors.blueAccent,
+            ),
+          ),
+          Positioned(
+            
+            child: Align(
+              
+                alignment: Alignment.bottomCenter,
+                child: Container(                  
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 99),
+                  child: Column(children: [
+                    Card(
+                      child: ListTile(
+                          leading: CircleAvatar(
+                            radius: 20.0,
+                            backgroundImage: NetworkImage(generalUrlAvatar),
+                          ),
+                          title: Text(name1),
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => GeneralPage(
+                                    currentUserId: myId,
+                                    friendId: idGeneral,
+                                    friendName: name1,
+                                    friendImage: generalUrlAvatar)));
+                          }),
+                    ),
+                    Card(
+                      child: ListTile(
+                          leading: CircleAvatar(
+                            radius: 20.0,
+                            backgroundImage: NetworkImage(generalUrlAvatar),
+                          ),
+                          title: Text(name2),
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => GeneralPage(
+                                    currentUserId: myId,
+                                    friendId: idGeneral,
+                                    friendName: name2,
+                                    friendImage: generalUrlAvatar)));
+                          }),
+                    ),
+                    Card(
+                      child: ListTile(
+                          leading: CircleAvatar(
+                            radius: 20.0,
+                            backgroundImage: NetworkImage(generalUrlAvatar),
+                          ),
+                          title: Text(name3),
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => GeneralPage(
+                                    currentUserId: myId,
+                                    friendId: idGeneral,
+                                    friendName: name3,
+                                    friendImage: generalUrlAvatar)));
+                          }),
+                    ),
+                    Card(
+                      child: ListTile(
+                          leading: CircleAvatar(
+                            radius: 20.0,
+                            backgroundImage: NetworkImage(myUrlAvatar),
+                          ),
+                          title: Text('Личные сообщения'),
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => OneChats(),
                             ));
-               }),
-               ListTile(
-                  leading: CircleAvatar(
-                    radius: 20.0,
-                    backgroundImage: NetworkImage(myUrlAvatar),
-                  ),
-                  title: Text('Личные сообщения'),
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => OneChats(),));
-               }),
-             ],
-           ), 
-    ));
+                          }),
+                    ),
+                  ]),
+                )),
+          ),
+        ]),
+      ]),
+    );
   }
+
   Widget buildUser(User user) {
     return ListTile(
       leading: CircleAvatar(
@@ -60,7 +137,6 @@ class _ChatsState extends State<Chats> {
       title: Text(user.name),
       subtitle: Text(user.description),
       onTap: () {
-        
         Navigator.of(context).push(MaterialPageRoute(
             builder: (context) => ChatPage(
                 currentUserId: myId,
--- lib/GeneralChats/Pages/GeneralChatsPage.dart
+++ lib/GeneralChats/Pages/GeneralChatsPage.dart
@@ -1,71 +1,71 @@
-import 'package:command_flutter/Chats/Data/Admin.dart';
-import 'package:command_flutter/GeneralChats/Data/General.dart';
-import 'package:command_flutter/GeneralChats/Pages/GeneralPage.dart';
-import 'package:flutter/material.dart';
+// import 'package:command_flutter/Chats/Data/Admin.dart';
+// import 'package:command_flutter/GeneralChats/Data/General.dart';
+// import 'package:command_flutter/GeneralChats/Pages/GeneralPage.dart';
+// import 'package:flutter/material.dart';
 
-class GeneralChatsPage extends StatefulWidget {
-  const GeneralChatsPage({super.key});
+// class GeneralChatsPage extends StatefulWidget {
+//   const GeneralChatsPage({super.key});
 
-  @override
-  State<GeneralChatsPage> createState() => _GeneralChatsPageState();
-}
+//   @override
+//   State<GeneralChatsPage> createState() => _GeneralChatsPageState();
+// }
 
-class _GeneralChatsPageState extends State<GeneralChatsPage> {
-  String name1 = 'Беседа для всех';
-  String name2 = 'Дом';
-  String name3 = 'Подъезд';
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-            backgroundColor: Colors.blueAccent, title: Text("Общий чат")),
-        body: SafeArea(
-          child: Column(
-            children: [
-              ListTile(
-                  leading: CircleAvatar(
-                    radius: 20.0,
-                    backgroundImage: NetworkImage(generalUrlAvatar),
-                  ),
-                  title: Text(name1),
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => GeneralPage(
-                            currentUserId: myId,
-                            friendId: idGeneral,
-                            friendName: name1,
-                            friendImage: generalUrlAvatar)));
-                  }),
-              ListTile(
-                  leading: CircleAvatar(
-                    radius: 20.0,
-                    backgroundImage: NetworkImage(generalUrlAvatar),
-                  ),
-                  title: Text(name2),
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => GeneralPage(
-                            currentUserId: myId,
-                            friendId: idGeneral,
-                            friendName: name2,
-                            friendImage: generalUrlAvatar)));
-                  }),
-              ListTile(
-                  leading: CircleAvatar(
-                    radius: 20.0,
-                    backgroundImage: NetworkImage(generalUrlAvatar),
-                  ),
-                  title: Text(name3),
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => GeneralPage(
-                            currentUserId: myId,
-                            friendId: idGeneral,
-                            friendName: name3,
-                            friendImage: generalUrlAvatar)));
-                  }),
-            ],
-          ),
-        ));
-  }
-}
+// class _GeneralChatsPageState extends State<GeneralChatsPage> {
+//   String name1 = 'Общий чат';
+//   String name2 = 'Дом';
+//   String name3 = 'Подъезд';
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//             backgroundColor: Colors.blueAccent, title: Text("Общий чат")),
+//         body: SafeArea(
+//           child: Column(
+//             children: [
+//               ListTile(
+//                   leading: CircleAvatar(
+//                     radius: 20.0,
+//                     backgroundImage: NetworkImage(generalUrlAvatar),
+//                   ),
+//                   title: Text(name1),
+//                   onTap: () {
+//                     Navigator.of(context).push(MaterialPageRoute(
+//                         builder: (context) => GeneralPage(
+//                             currentUserId: myId,
+//                             friendId: idGeneral,
+//                             friendName: name1,
+//                             friendImage: generalUrlAvatar)));
+//                   }),
+//               ListTile(
+//                   leading: CircleAvatar(
+//                     radius: 20.0,
+//                     backgroundImage: NetworkImage(generalUrlAvatar),
+//                   ),
+//                   title: Text(name2),
+//                   onTap: () {
+//                     Navigator.of(context).push(MaterialPageRoute(
+//                         builder: (context) => GeneralPage(
+//                             currentUserId: myId,
+//                             friendId: idGeneral,
+//                             friendName: name2,
+//                             friendImage: generalUrlAvatar)));
+//                   }),
+//               ListTile(
+//                   leading: CircleAvatar(
+//                     radius: 20.0,
+//                     backgroundImage: NetworkImage(generalUrlAvatar),
+//                   ),
+//                   title: Text(name3),
+//                   onTap: () {
+//                     Navigator.of(context).push(MaterialPageRoute(
+//                         builder: (context) => GeneralPage(
+//                             currentUserId: myId,
+//                             friendId: idGeneral,
+//                             friendName: name3,
+//                             friendImage: generalUrlAvatar)));
+//                   }),
+//             ],
+//           ),
+//         ));
+//   }
+// }
