1181a42f4adfb162e4f346f2d5d2bc2167a36848
sweet62 <maks_shuyanov@mail.ru>
2022-11-15 21:26:42 +0300

    add

--- lib/Pages/ProfilePage.dart
+++ lib/Pages/ProfilePage.dart
@@ -10,7 +10,7 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter/services.dart';
 
-import 'TestPage.dart';
+import '../Widget/ImageProfileWidget.dart';
 
 class ProfileePage extends StatefulWidget {
   const ProfileePage({Key? key}) : super(key: key);
@@ -34,120 +34,122 @@ class _ProfileePageState extends State<ProfileePage> {
 
     return ListView(
       children: [
-    Stack(
-    children: <Widget>
-    [
-    Container(
-    child: Stack(
-    children: [
-    Image.asset('assets/profile/profileBackground.jpg',width: 1000, fit:BoxFit.fill),
-    Image.asset('assets/profile/GroundUpBar.png',width: 1000, fit:BoxFit.fill),
-    ],
-    ),
-    ),
-    Column(
-    crossAxisAlignment: CrossAxisAlignment.stretch,
-    children: [
-    Container(
-    child: Stack(
-    children: [
-    WidgetProfileName(),
-    ],
-    )
-    ),
-    SizedBox(height: 90),
-    Padding(
-    padding: EdgeInsets.only(left: 20,right: 20,bottom: 0 ),
-    child: Container(
-    height: 40,
-    width: 330,
-    child: buildPersonalBillButton(),
-    ),
-    ),
-    ////
-    SizedBox(height: 50),
-    Center(
-    child: Expanded(
-    child: Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-    Stack(
-    children: [
-    Container(
-    height: 80,
-    width: 80,
-    child: build_UK_Button(),
-    ),
-    ],
-    ),
-    SizedBox(width: 30),
-    Container(
-    height: 80,
-    width: 80,
-    child: build_Problem_Button(),
-    ),
-    SizedBox(width: 30),
-    Container(
-    height: 80,
-    width: 80,
-    child:  build_worker_Button(),
-    )
-    ],
-    )),
-    ),
-    SizedBox(height: 5),
-    Center(
-    child: Expanded(
-    child: Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-    SizedBox(width: 30),
-    Column(
-    children: [
-    Text("Обращение", style: TextStyle(fontSize: 14, color: Colors.grey)),
-    Text("в УК", style: TextStyle(fontSize: 14, color: Colors.grey)),
-    ],
-    ),
-    SizedBox(width: 30),
-    Column(
-    children: [
-    Text("Сообщить", style: TextStyle(fontSize: 14, color: Colors.grey)),
-    Text("О проблеме", style: TextStyle(fontSize: 14, color: Colors.grey)),
-    ],
-    ),
-    SizedBox(width: 20),
-    Column(
-    children: [
-    Text("Вызов  ", style: TextStyle(fontSize: 14, color: Colors.grey)),
-    Text("Ремонтых Служб", style: TextStyle(fontSize: 14, color: Colors.grey)),
-    ],
-    ),
-    ],
-    )),
-    ),
-    SizedBox(height: 30),
-    ////
-    Padding(
-    padding: EdgeInsets.only(left: 20,right: 20,bottom: 10 ),
-    child: Container(
-    height: 40,
-    width: 330,
-    child: build_Setting_Button(),
-    )
-    ),
-    SizedBox(height: 10),
-    Padding(
-    padding: EdgeInsets.only(left: 20,right: 20,bottom: 10 ),
-    child: Container(
-    height: 40,
-    width: 330,
-    child: buildExitProfileButton(),
-    )
-    )
-    ],
-    )
-    ],
-    ),
+      Stack(
+        children: <Widget>
+            [
+            Container(
+            child: Stack(
+            children: [
+              Image.asset('assets/profile/profileBackground.jpg',width: 1000, fit:BoxFit.fill),
+              Image.asset('assets/profile/GroundUpBar.png',width: 1000, fit:BoxFit.fill),
+              ],
+              ),
+            ),
+              Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  child: Stack(
+                    children: [
+                      WidgetProfileName(),
+                    ],
+              )
+            ),
+                  SizedBox(height: 90),
+                  //Button
+                  Padding(
+                    padding: EdgeInsets.only(left: 20,right: 20,bottom: 0 ),
+                        child: Container(
+                        height: 40,
+                        width: 330,
+                        child: buildPersonalBillButton(),
+                    ),
+                  ),
+                ////
+                  SizedBox(height: 50),
+                  Center(
+                  child: Expanded(
+                    child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                    Stack(
+                    children: [
+                      Container(
+                        height: 80,
+                        width: 80,
+                        child: build_UK_Button(),
+                        ),
+                    ],
+                  ),
+                  SizedBox(width: 30),
+                  Container(
+                    height: 80,
+                    width: 80,
+                    child: build_Problem_Button(),
+                  ),
+                  SizedBox(width: 30),
+                  Container(
+                    height: 80,
+                    width: 80,
+                    child:  build_worker_Button(),
+                    )
+                        ],
+                      )
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Center(
+                  child: Expanded(
+                  child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                  SizedBox(width: 30),
+                  Column(
+                  children: [
+                  Text("Обращение", style: TextStyle(fontSize: 14, color: Colors.grey)),
+                  Text("в УК", style: TextStyle(fontSize: 14, color: Colors.grey)),
+                  ],
+                  ),
+                  SizedBox(width: 30),
+                  Column(
+                  children: [
+                  Text("Сообщить", style: TextStyle(fontSize: 14, color: Colors.grey)),
+                  Text("О проблеме", style: TextStyle(fontSize: 14, color: Colors.grey)),
+                  ],
+                  ),
+                  SizedBox(width: 20),
+                  Column(
+                  children: [
+                  Text("Вызов  ", style: TextStyle(fontSize: 14, color: Colors.grey)),
+                  Text("Ремонтых Служб", style: TextStyle(fontSize: 14, color: Colors.grey)),
+                  ],
+                  ),
+                  ],
+                  )),
+                  ),
+                  SizedBox(height: 30),
+                  ////
+                  Padding(
+                  padding: EdgeInsets.only(left: 20,right: 20,bottom: 10 ),
+                  child: Container(
+                  height: 40,
+                  width: 330,
+                  child: build_Setting_Button(),
+                  )
+                  ),
+                  SizedBox(height: 10),
+                  Padding(
+                  padding: EdgeInsets.only(left: 20,right: 20,bottom: 10 ),
+                  child: Container(
+                  height: 40,
+                  width: 330,
+                  child: buildExitProfileButton(),
+                  )
+                  )
+               ],
+              )
+          ],
+        ),
       ],
     );
   }
@@ -197,7 +199,7 @@ class _ProfileePageState extends State<ProfileePage> {
     imagees: "assets/profile/Massage.png",
     onClicked: (){
       Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => SecondScreen())
+          MaterialPageRoute(builder: (context) => FirstRoute())
       );
     },
   );
@@ -206,7 +208,7 @@ class _ProfileePageState extends State<ProfileePage> {
     imagees: "assets/profile/SosButton.png",
     onClicked: (){
       Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => SecondScreen())
+          MaterialPageRoute(builder: (context) => FirstRoute())
       );
     },
   );
@@ -215,7 +217,7 @@ class _ProfileePageState extends State<ProfileePage> {
       imagees: "assets/profile/IconMan.png",
     onClicked: (){
       Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => SecondScreen())
+          MaterialPageRoute(builder: (context) => FirstRoute())
       );
     },
   );
--- lib/Pages/TestPage.dart
+++ lib/Pages/TestPage.dart
@@ -1,151 +1,85 @@
 import 'dart:io';
-import 'package:command_flutter/Pages/ProfilePage.dart';
-import 'package:flutter/material.dart';
 import 'package:editable_image/editable_image.dart';
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
-
-class SecondScreen extends StatefulWidget {
-
-
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
   @override
-  State<SecondScreen> createState() => _SecondScreenState();
+  _HomeViewState createState() => _HomeViewState();
 }
 
-class _SecondScreenState extends State<SecondScreen> {
+class _HomeViewState extends State<HomeView> {
+  late Directory _appDocDir;
   File? _profilePicFile;
+  bool Flag = true;
 
   @override
   void initState() {
+    final Flag;
     super.initState();
+    getApplicationDocumentsDirectory().then((appDocDir) {
+      _appDocDir = appDocDir;
+      _profilePicFile = File(join(appDocDir.path, 'profile_picture.png'));
+      setState(() {});
+    });
   }
-
-  // A simple usage of EditableImage.
-  void _directUpdateImage(File? file) async {
+  void _saveImageToAppDirectoryForNextUses(File? file) async {
     if (file == null) return;
-    setState(() {
-      _profilePicFile = file;
+
+    if (_profilePicFile != null) {
+      await _profilePicFile!.delete(recursive: true);
+
+      setState(() {
+        _profilePicFile = null;
+      });
+    }
+
+    imageCache.clear();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      _profilePicFile = File(join(_appDocDir.path, 'profile_picture.png'));
+      await file.copy(_profilePicFile!.path);
+      setState(() {});
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Text(
-                'Profile Settings',
-                style: TextStyle(
-                  fontSize: 28.0,
-                ),
-              ),
-              const Spacer(flex: 2),
-              EditableImage(
-                // Define the method that will run on the change process of the image.
-                onChange: (file) => _directUpdateImage(file),
-                // Define the source of the image.
-                image: _profilePicFile != null
-                    ? Image.file(_profilePicFile!, fit: BoxFit.cover)
-                    : null,
-                // Define the size of EditableImage.
-                size: 150.0,
+    return Container(
+      child: EditableImage(
+        // Define the method that will run on the change process of the image.
+        onChange: (file) => _saveImageToAppDirectoryForNextUses(file),
 
-                // Define the Theme of image picker.
-                imagePickerTheme: ThemeData(
-                  // Define the default brightness and colors.
-                  primaryColor: Colors.white,
-                  shadowColor: Colors.transparent,
-                  backgroundColor: Colors.yellowAccent,
-                  iconTheme: const IconThemeData(color: Colors.yellowAccent),
+        // Define the source of the image.
+        image: _profilePicFile != null
+            ? Image.file(_profilePicFile!, fit: BoxFit.cover)
+            : null,
 
-                  // Define the default font family.
-                  fontFamily: 'Georgia',
-                ),
+        // Define the size of EditableImage.
+        size: 120.0,
 
-                // Define the border of the image if needed.
-                imageBorder: Border.all(color: Colors.black, width: 2.0),
-                // Define the border of the icon if needed.
-                editIconBorder: Border.all(color: Colors.black, width: 2.0),
-              ),
+        // Define the position of edit icon.
+        editIconPosition: Position.bottomLeft,
 
-              const Spacer(flex: 2),
-              _buildTextField(labelText: 'Username'),
-              const Spacer(),
-              _buildTextField(labelText: 'Full Name'),
-              const Spacer(),
-              _buildTextField(labelText: 'Email'),
-              const Spacer(),
-              _buildTextField(labelText: 'Password', obscureText: true),
-              const Spacer(flex: 2),
-              _buildTextButton(),
-            ],
-          ),
+        // Define the Theme of image picker.
+        imagePickerTheme: ThemeData(
+          // Define the default brightness and colors.
+          primaryColor: Colors.white,
+          shadowColor: Colors.transparent,
+          backgroundColor: Colors.white70,
+          iconTheme: const IconThemeData(color: Colors.black87),
+          // Define the default font family.
+          fontFamily: 'Georgia',
         ),
+
+        // Define the border of the image if needed.
+        imageBorder: Border.all(color: Colors.black87, width: 2.0),
+
+        // Define the border of the icon if needed.
+        editIconBorder: Border.all(color: Colors.black87, width: 2.0),
       ),
-    );
+      );
   }
-}
-
-TextField _buildTextField({String labelText = '', bool obscureText = false}) {
-  return TextField(
-    cursorColor: Colors.yellowAccent,
-    cursorWidth: 1.0,
-    obscureText: obscureText,
-    obscuringCharacter: '●',
-    decoration: InputDecoration(
-      labelText: labelText,
-      labelStyle: const TextStyle(
-        color: Colors.black54,
-        fontSize: 18.0,
-      ),
-      fillColor: Colors.red,
-      border: const OutlineInputBorder(
-        borderSide: BorderSide(
-          color: Colors.black54,
-        ),
-        borderRadius: BorderRadius.all(
-          Radius.circular(40.0),
-        ),
-      ),
-      focusedBorder: const OutlineInputBorder(
-        borderSide: BorderSide(
-          color: Colors.black54,
-          width: 1.5,
-        ),
-        borderRadius: BorderRadius.all(
-          Radius.circular(40.0),
-        ),
-      ),
-    ),
-  );
-}
-
-TextButton _buildTextButton() {
-  return TextButton(
-    onPressed: () => {
-      MaterialPageRoute(
-        builder: (context) {
-          return ProfileePage();
-        },
-      ),
-    },
-    style: ButtonStyle(
-      padding: MaterialStateProperty.all(
-        const EdgeInsets.symmetric(vertical: 20.0),
-      ),
-      side:
-      MaterialStateProperty.all(const BorderSide(color: Colors.black54)),
-      backgroundColor: MaterialStateProperty.all(Colors.transparent),
-    ),
-    child: const Text(
-      'Save',
-      style: TextStyle(
-        color: Colors.black,
-        fontSize: 18.0,
-      ),
-    ),
-  );
 }
